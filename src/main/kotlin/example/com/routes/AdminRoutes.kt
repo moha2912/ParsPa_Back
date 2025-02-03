a5711f731df2f414d26921b2dabb365732095c9c
Mahdi <self.mmp@gmail.com>
2025-02-03 16:56:29 +0330

    Develop

--- src/main/kotlin/example/com/routes/AdminRoutes.kt
+++ src/main/kotlin/example/com/routes/AdminRoutes.kt
@@ -62,7 +62,7 @@ fun Route.adminRoutes(
 ) {
     route("/admin") {
         get("/updateArticle") {
-            sendExecuteCommand("update")//update_article
+            sendExecuteCommand("update_article")//update_article
         }
 
         get("/updatePwa") {
