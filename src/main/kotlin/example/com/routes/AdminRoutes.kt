ebb5d3fec46aaa0304e479a8780406ffe8cd430a
Mahdi <self.mmp@gmail.com>
2025-02-10 16:12:14 +0330

    Develop

--- src/main/kotlin/example/com/routes/AdminRoutes.kt
+++ src/main/kotlin/example/com/routes/AdminRoutes.kt
@@ -92,18 +92,7 @@ fun Route.adminRoutes(
 
         get("/resetBot") {
             if (isDebug) {
-                executeCommand("/bin/bash /root/reset_proxy.sh") {
-                    TelegramBot.sendBotMessage()
-                    call.respondText(
-                        """
-                    <html>
-                        <body>
-                            <h1>Resetting bot...</h1>
-                        </body>
-                    </html>
-                """, ContentType.Text.Html
-                    )
-                }
+                TelegramBot.sendRawMessage("vsdfbgnh")
                 return@get
             }
             if (!isDebug) {
